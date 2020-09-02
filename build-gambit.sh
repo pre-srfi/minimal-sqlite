21373481a0c082be9a99fc72ba0b8f64ee4cc18d
Lassi Kortela <lassi@lassi.io>
2020-09-02 10:08:41 +0300

    Build with debug info

--- build-gambit.sh
+++ build-gambit.sh
@@ -3,4 +3,4 @@ set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 set -x
-gsc-script -exe -o test . minimal-sqlite.sld test.scm
+gsc-script -exe -debug -o test . minimal-sqlite.sld test.scm
